f8365fae65f48253dbcf210581ca2b376ea98c32
deltamoe <154552217+deltamoe@users.noreply.github.com>
2023-12-26 18:15:52 +0100

    Add files via upload

---
+++ character.py
@@ -0,0 +1,26 @@
+class Character:
+    
+    def __init__(self, canvas):
+        
+        self.canvas = canvas
+        self._hp = None
+        self._x_pos = None
+        self._y_pos = None
+        self._damage = None
+        
+    
+    def set_hp(self):
+        
+    def set_x_pos(self):
+        
+    def set_y_pos(self):
+    
+    def set_damage(self):
+        
+    def drop_item(self):
+        """
+        executes the dropped() method of Item
+        """
+    
+    
+    
